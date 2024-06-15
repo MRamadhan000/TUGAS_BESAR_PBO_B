af838a076d911f54553e83413e23a41246e93da8
unknown <mcahya830@gmail.com>
2024-06-15 15:55:34 +0700

    first commit

---
+++ src/main/java/org/example/com/main/exception/custom/illegalAdminAcces.java
@@ -0,0 +1,9 @@
+package org.example.com.main.exception.custom;
+
+public class illegalAdminAcces extends Exception{
+    public illegalAdminAcces(String message){
+        super(message);
+    }
+
+
+}
